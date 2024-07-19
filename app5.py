10c17d0ea20d250d0a347d1b9b6f364a22fbc378
princevalerie <148141403+princevalerie@users.noreply.github.com>
2024-07-19 18:00:37 +0700

    Update app5.py

--- app5.py
+++ app5.py
@@ -125,7 +125,7 @@ if image_data is not None:
             char_image_pil = Image.fromarray(char_image)
             char_class = predict(char_image_pil, model, transform)
             word += char_class
-            if i < len(spaces) and spaces[i] > 25:
+            if i < len(spaces) and spaces[i] > 30:
                 recognized_text += word + " "
                 word = ""
         
