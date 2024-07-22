3b4db8ee65fcb8497a562516f060fb5823a54c0c
princevalerie <148141403+princevalerie@users.noreply.github.com>
2024-07-23 01:03:43 +0700

    Update app5.py

--- app5.py
+++ app5.py
@@ -106,7 +106,7 @@ def count_chars_left_of_spaces(positions, valid_chars):
 # Function to add spaces to characters
 def add_spaces_to_chars(segmented_chars, positions, char_counts_left_of_spaces):
     result = []
-    char_index = 0
+    char_index = -1
     for i, (char_image, x) in enumerate(segmented_chars):
         result.append((char_image, x))
         # Check if a space should be added
